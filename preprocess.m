6cff13840e3e67b07bbb922a20ab14d567e8e015
zqwang <zqwang9@stanford.edu>
2026-02-21 19:41:20 -0800

    add EXTRACT with license

--- preprocess.m
+++ preprocess.m
@@ -1,11 +1,42 @@
 % function [out] = preprocess(h5Path)
 
     addpath('./dependencies');
+    addpath('./dependencies/EXTRACT-public');
+    addpath('./dependencies/EXTRACT-public/EXTRACT');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/debug_utils');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/debug_utils/brewermap');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/helper_functions');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/solvers');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/solvers/base solvers for multiple cells');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/solvers/cell finding solvers');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/main_functions/solvers/cell refinement + frr solvers');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/modules');
+    addpath('./dependencies/EXTRACT-public/EXTRACT/summary functions');
+    addpath('./dependencies/EXTRACT-public/External algorithms');
+    addpath('./dependencies/EXTRACT-public/External algorithms/Template scripts');
+    addpath('./dependencies/EXTRACT-public/External algorithms/parfor_progress');
+    addpath('./dependencies/EXTRACT-public/External algorithms/progressbar');
+    addpath('./dependencies/EXTRACT-public/External algorithms/simulation_utils');
+    addpath('./dependencies/EXTRACT-public/Learning-materials');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Additional demos');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Additional demos/NWB Demos');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Additional demos/Neurofinder_training_0200');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Additional demos/SVD-denoising for low SNR movies');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Additional demos/Simulating movies');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-1-Starting-code');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-2-Parallelization');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-3-Preprocessing');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-4-Cellfinding');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-5-Cell-refinement');
+    addpath('./dependencies/EXTRACT-public/Learning-materials/Lecture-Tutorials/Tutorial-6-Final-robust-regression');
     addpath('./dependencies/PCAICA');
     addpath('./dependencies/PCAICA/doc');
     addpath('./dependencies/PCAICA/doc/images');
     addpath('./dependencies/altmany-export_fig');
     addpath('./dependencies/altmany-export_fig/.ignore');
+    
     addpath('./preprocessing');
     addpath('./preprocessing/demixing');
     addpath('./preprocessing/denoising');
@@ -21,11 +52,13 @@
     addpath('./preprocessing/loading/dcimg');
     addpath('./preprocessing/metadata');
     addpath('./preprocessing/motionCorr');
+    
     addpath('./analysis');
     addpath('./analysis/cleanCells');
     addpath('./analysis/plots');
     addpath('./analysis/spikeTiming');
     addpath('./analysis/unmixing');
+    
     addpath('./utilities');
     addpath('./utilities/paths');
     addpath('./utilities/saving');
